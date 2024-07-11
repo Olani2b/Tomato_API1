c8ffd25981f81c9d43d912c12b47b85d268550af
Olani2b <olanibaissa@gmail.com>
2024-07-11 15:04:19 +0200

    Add test to count the number of recipes for Tomato Bistro

--- tomato/myapp/tests.py
+++ tomato/myapp/tests.py
@@ -30,8 +30,13 @@ class RecipeTests(APITestCase):
 class RestaurantTests(APITestCase):
     def setUp(self):
         self.ingredient = Ingredient.objects.create(name='Tomato')
-        self.recipe = Recipe.objects.create(name='Tomato Soup')
-        self.recipe.ingredients.add(self.ingredient)
+        self.recipe1 = Recipe.objects.create(name='Tomato Soup')
+        self.recipe1.ingredients.add(self.ingredient)
+        self.recipe2 = Recipe.objects.create(name='Tomato Salad')
+        self.recipe2.ingredients.add(self.ingredient)
+
+        self.restaurant = Restaurant.objects.create(name='Tomato Bistro')
+        self.restaurant.recipes.add(self.recipe1, self.recipe2)
 
     def test_create_restaurant(self):
         url = reverse('restaurant-list')
@@ -41,5 +46,16 @@ class RestaurantTests(APITestCase):
         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Restaurant.objects.count(), 1)
-        self.assertEqual(Restaurant.objects.get().name, 'Tomato Bistro')
+        self.assertEqual(Restaurant.objects.count(), 2)  # There should be 2 restaurants now
+        self.assertEqual(Restaurant.objects.latest('id').name, 'Tomato Bistro')
+
+    def test_restaurant_recipe_count(self):
+        url = reverse('restaurant-detail', args=[self.restaurant.id])
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['recipes']), 2)
+
+    def test_count_recipes_for_tomato_bistro(self):
+        
+        restaurant = Restaurant.objects.get(name='Tomato Bistro')
+        self.assertEqual(restaurant.recipes.count(), 2)
